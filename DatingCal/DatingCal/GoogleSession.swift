0d75d6cbcbdc7abe05c28b6f1a683491db3d3469
C4Phone <qq2900726136@gmail.com>
2017-04-11 16:25:22 -0500

    finished encapsulation

---
+++ DatingCal/DatingCal/GoogleSession.swift
@@ -0,0 +1,84 @@
+//
+//  GoogleSession.swift
+//  DatingCal
+//
+//  Created by Zhongzhi Yu on 4/11/17.
+//
+//
+
+import Foundation
+import AppAuth
+import PromiseKit
+import GoogleAPIClientForREST
+import SwiftyJSON
+import RealmSwift
+
+/// Manages a login session with some Google user.
+/// It manages the cache of login tokens and decide whether it's needed to start login process.
+class GoogleSession {
+    private let kScopes : [String]? = ["https://www.googleapis.com/auth/calendar"]
+    private let kRedirectURI : URL = URL(string: "cs242.datingcal:/oauth2redirect/google")!
+    private let kClientId = "674497672844-d33bqapee8lm5l90l021sml0nsbvu3qp.apps.googleusercontent.com"
+    
+    private let googleAuth = OIDPromise(issuer: URL(string: "https://accounts.google.com")!)
+    private var googleAuthStateStorage : String {
+        get {
+            let library = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]
+            return library + "/google-auth-state.dat"
+        }
+    }
+    
+    private var _authState : OIDAuthState?
+    
+    var authState : OIDAuthState? { get {return _authState} }
+    var token : String? { get {return authState?.lastTokenResponse?.accessToken} }
+    
+    private func saveAuthState() {
+        if let state = _authState {
+            NSKeyedArchiver.archiveRootObject(state, toFile: googleAuthStateStorage)
+        }
+    }
+    
+    private func loadAuthState() {
+        guard let data = NSData(contentsOfFile: googleAuthStateStorage) else {
+            return
+        }
+        let unarchiver = NSKeyedUnarchiver(forReadingWith: data as Data)
+        unarchiver.requiresSecureCoding = false
+        self._authState = unarchiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as? OIDAuthState
+    }
+    
+    func ensureLogin(presenter: UIViewController) -> Promise<Void> {
+        loadAuthState()
+        if self.authState != nil {
+            return when(fulfilled: [])
+        } else {
+            return googleAuth.getConfigurations().then { config -> Promise<OIDAuthState> in
+                let request = OIDAuthorizationRequest(configuration: config
+                    , clientId: self.kClientId, clientSecret: nil
+                    , scopes: self.kScopes, redirectURL: self.kRedirectURI
+                    , responseType: OIDResponseTypeCode, additionalParameters: nil)
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let (flow, promise) = self.googleAuth.authState(request: request, presenter: presenter)
+                appDelegate.googleAuthFlow = flow
+                return promise
+            }.then { authState -> Void in
+                self._authState = authState
+                self.saveAuthState()
+            }
+        }
+    }
+    
+    var isLoggedIn : Bool {
+        get {
+            return authState != nil
+        }
+    }
+    
+    func logout() {
+        self._authState = nil
+        try? FileManager.default.removeItem(atPath: googleAuthStateStorage)
+    }
+    
+}
--- DatingCal/DatingCal/LoginViewController.swift
+++ DatingCal/DatingCal/LoginViewController.swift
@@ -16,70 +16,29 @@ import RealmSwift
 
 class LoginViewController: UIViewController {
     
-    private let kScopes : [String]? = ["https://www.googleapis.com/auth/calendar"]
-    private let kRedirectURI : URL = URL(string: "cs242.datingcal:/oauth2redirect/google")!
-    private let kClientId = "674497672844-d33bqapee8lm5l90l021sml0nsbvu3qp.apps.googleusercontent.com"
+    private let googleSession = GoogleSession()
+    private var googleCalendar : GoogleCalendar?
     
-    let googleAuth = OIDPromise(issuer: URL(string: "https://accounts.google.com")!)
-    var googleAuthStateStorage : String {
-        get {
-            let library = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]
-            return library + "/google-auth-state.dat"
-        }
-    }
+    private var pendingSignIn : Promise<Void>?
     
-    var authState : OIDAuthState?
-    var token : String?
-    var googleCalendar : GoogleCalendar?
-    
-    func saveAuthState() {
-        if let state = authState {
-            NSKeyedArchiver.archiveRootObject(state, toFile: googleAuthStateStorage)
-        }
-    }
-    
-    func loadAuthState() {
-        guard let data = NSData(contentsOfFile: googleAuthStateStorage) else {
+    @IBAction func willSignIn(_ sender: Any) {
+        if pendingSignIn != nil {
+            print("Please wait for current login process to finish.")
             return
         }
-        let unarchiver = NSKeyedUnarchiver(forReadingWith: data as Data)
-        unarchiver.requiresSecureCoding = false
-        self.authState = unarchiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as? OIDAuthState
-    }
-    
-    func afterLogin() -> Promise<Void> {
-        self.token = self.authState?.lastTokenResponse?.accessToken
-        self.googleCalendar = GoogleCalendar(self.token!)
-        return self.googleCalendar!.loadAll().then { x -> Void in
+        pendingSignIn = self.googleSession.ensureLogin(presenter: self).then { x -> Promise<Void> in
+            print("Sign in finished")
+            self.googleCalendar = GoogleCalendar(self.googleSession.token!)
+            return self.googleCalendar!.loadAll()
+        }.then { x -> Void in
             var realm = try! Realm()
             for cal in realm.objects(CalendarModel.self) {
                 print(cal)
             }
         }.catch { err -> Void in
             print("ERROR: ", err)
-        }
-    }
-    
-    @IBAction func willSignIn(_ sender: Any) {
-        loadAuthState()
-        if let authState = self.authState {
-            afterLogin()
-        } else {
-            googleAuth.getConfigurations().then { config -> Promise<OIDAuthState> in
-                let request = OIDAuthorizationRequest(configuration: config
-                    , clientId: self.kClientId, clientSecret: nil
-                    , scopes: self.kScopes, redirectURL: self.kRedirectURI
-                    , responseType: OIDResponseTypeCode, additionalParameters: nil)
-                
-                let appDelegate = UIApplication.shared.delegate as! AppDelegate
-                let (flow, promise) = self.googleAuth.authState(request: request, presenter: self)
-                appDelegate.googleAuthFlow = flow
-                return promise
-            }.then { authState -> Promise<Void> in
-                self.authState = authState
-                self.saveAuthState()
-                return self.afterLogin()
-            }
+        }.always {
+            self.pendingSignIn = nil
         }
     }
     
