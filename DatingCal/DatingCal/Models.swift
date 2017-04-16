f7116f127da286ce1f67f2a57c092e00c6e15677
C4Phone <qq2900726136@gmail.com>
2017-04-16 17:49:22 -0500

    created 'add event' API. Currently it only works in online mode

--- DatingCal/DatingCal/Models.swift
+++ DatingCal/DatingCal/Models.swift
@@ -10,32 +10,48 @@ import Foundation
 import SwiftyJSON
 import RealmSwift
 import SwiftDate
+import Alamofire
 
-class CalendarModel : Object {
+protocol GoogleParsable {
+    func parse(_ json: JSON)
+    func unParse() -> Parameters
+}
+
+class CalendarModel : Object, GoogleParsable {
+
     dynamic var id: String = ""
     dynamic var name: String = ""
     dynamic var timeZone: String? = nil
     dynamic var bgColor: String? = nil
     dynamic var isPrimary: Bool = false
-    let events = List<EventModel>()
+    var events = List<EventModel>()
     
     override class func primaryKey() -> String? {
         return "id"
     }
     
-    /// Create a CalendarModel from a JSON returned from Google
-    static func parse(_ json: JSON) -> CalendarModel {
-        var ans = CalendarModel()
-        ans.id = json["id"].string!
-        ans.name = json["summary"].string!
-        ans.timeZone = json["timeZone"].string
-        ans.bgColor = json["backgroundColor"].string
-        ans.isPrimary = json["primary"].bool ?? false
+    func unParse() -> Parameters {
+        var ans : Parameters = [:]
+        ans["summary"] = name
+        ans["timeZone"] = timeZone
         return ans
+    }
+    
+    /// Create a CalendarModel from a JSON returned from Google
+    func parse(_ json: JSON) {
+        let originalId = id
+        id = json["id"].string!
+        name = json["summary"].string!
+        timeZone = json["timeZone"].string
+        bgColor = json["backgroundColor"].string
+        isPrimary = json["primary"].bool ?? false
+        if originalId != id {
+            events = List<EventModel>()
+        }
     }
 }
 
-class EventModel : Object {
+class EventModel : Object, GoogleParsable {
     dynamic var id: String = ""
     dynamic var summary: String = ""
     dynamic var desc: String = ""
@@ -52,7 +68,7 @@ class EventModel : Object {
     
     /// A helper function that parses any substructure that represents a time/date
     ///   json: Please pass in the substructure that directly contains "dateTime" or "date"
-    private static func parseDates(_ json: JSON) -> (Date?, Date?, String?) {
+    private func parseDates(_ json: JSON) -> (Date?, Date?, String?) {
         var date : Date? = nil
         var time : Date? = nil
         var timeZone : String? = nil
@@ -68,20 +84,42 @@ class EventModel : Object {
         return (date, time, timeZone)
     }
     
+    private func unParseDates(_ date: Date?, _ time: Date?, _ timeZone: String?) -> Parameters {
+        var ans : Parameters = [:]
+        ans["date"] = date?.string(format: DateFormat.custom("yyyy-MM-dd"))
+        ans["timeZone"] = timeZone
+        
+        if let timeZone = timeZone, let time = time {
+            var dateTimeFormat = ISO8601DateFormatter()
+            dateTimeFormat.timeZone = TimeZone(identifier: timeZone)
+            ans["dateTime"] = dateTimeFormat.string(from: time)
+        }
+        return ans
+    }
+    
+    func unParse() -> Parameters {
+        var ans : Parameters = [:]
+        var startDict : Parameters = unParseDates(startDate, startTime, startTimeZone)
+        var endDict : Parameters = unParseDates(endDate, endTime, endTimeZone)
+        ans["summary"] = summary
+        ans["description"] = desc
+        ans["start"] = startDict
+        ans["end"] = endDict
+        return ans
+    }
+    
     /// Create a EventModel from a JSON returned from Google
-    static func parse(_ json: JSON) -> EventModel {
-        var ans = EventModel()
+    func parse(_ json: JSON) {
         let start = parseDates(json["start"])
         let end = parseDates(json["end"])
-        ans.id = json["id"].string!
-        ans.summary = json["summary"].string ?? ""
-        ans.desc = json["description"].string ?? ""
-        ans.startDate = start.0
-        ans.startTime = start.1
-        ans.startTimeZone = start.2
-        ans.endDate = end.0
-        ans.endTime = end.1
-        ans.endTimeZone = end.2
-        return ans
+        id = json["id"].string!
+        summary = json["summary"].string ?? ""
+        desc = json["description"].string ?? ""
+        startDate = start.0
+        startTime = start.1
+        startTimeZone = start.2
+        endDate = end.0
+        endTime = end.1
+        endTimeZone = end.2
     }
 }
