e1d9d8ce5c9ded8baa75ab97a74d7d6f609f7eb8
C4Phone <qq2900726136@gmail.com>
2017-05-02 17:50:59 -0500

    simplify design of details view, even further

--- DatingCal/DatingCal/EventDetailViewController.swift
+++ DatingCal/DatingCal/EventDetailViewController.swift
@@ -9,32 +9,9 @@
 import UIKit
 
 class EventDetailViewController: UIViewController {
-
-    /// set status bar to white color
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+    @IBAction func willCloseWindow(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
