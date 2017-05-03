5a2796556851c2f3b92d1b957d3415c204647f83
C4Phone <qq2900726136@gmail.com>
2017-05-03 11:00:03 -0500

    complete test cases

--- DatingCal/DatingCalTests/EventModelTests.swift
+++ DatingCal/DatingCalTests/EventModelTests.swift
@@ -50,22 +50,116 @@ class EventModelTests : AsyncTests {
         }
     }
     
-    func testDescriptionForEmptyAllDayEvent() {
+    private func testTimeForAllDayEvent(recurring: Bool) {
         let start = "2016 April 10".date(format: DateFormat.custom("yyyy MMMM d"))
         let end = "2016 April 11".date(format: DateFormat.custom("yyyy MMMM d"))
         let realm = realmProvider.realm()
         try! realm.write {
             event.startDate = start!.absoluteDate
             event.endDate = end!.absoluteDate
+            if recurring {
+                event.setRecurrence(newValue: ["fake rule"])
+            }
         }
         
         guard let desc = event.describe() else {
             XCTFail()
             return
         }
-        XCTAssertTrue(desc.contains("This is an all-day, non-recurring event"))
+        if recurring {
+            XCTAssertTrue(desc.contains("This is an all-day, recurring event"))
+        } else {
+            XCTAssertTrue(desc.contains("This is an all-day, non-recurring event"))
+        }
         XCTAssertTrue(desc.contains("It starts from Sunday, April 10, 2016"))
         XCTAssertTrue(desc.contains("It ends on Monday, April 11, 2016"))
-        XCTAssertTrue(desc.contains("And the author didn't write anything else"))
+    }
+    
+    private func testTimeForNonAllDayEvent(recurring: Bool) {
+        let start = "2016 April 10 20:10".date(format: DateFormat.custom("yyyy MMMM d hh:mm"))
+        let end = "2016 April 11 21:22".date(format: DateFormat.custom("yyyy MMMM d hh:mm"))
+        let realm = realmProvider.realm()
+        try! realm.write {
+            event.startTime = start!.absoluteDate
+            event.endTime = end!.absoluteDate
+            if recurring {
+                event.setRecurrence(newValue: ["fake rule"])
+            }
+        }
+        
+        guard let desc = event.describe() else {
+            XCTFail()
+            return
+        }
+        if recurring {
+            XCTAssertTrue(desc.contains("This is a recurring event"))
+        } else {
+            XCTAssertTrue(desc.contains("This is a non-recurring event"))
+        }
+        XCTAssertTrue(desc.contains("It starts from Sun, April 10, 2016 8:10 PM"))
+        XCTAssertTrue(desc.contains("It ends on Monday, April 11, 2016 9:22 PM"))
+    }
+    
+    private func testDescriptionForExtraDescription(desc: String? = nil) {
+        guard let _desc = desc else {
+            /// Test event without extra description
+            guard let desc = event.describe() else {
+                XCTFail()
+                return
+            }
+            XCTAssertTrue(desc.contains("And the author didn't write anything else"))
+            return
+        }
+        
+        // Test event with extra description
+        let realm = realmProvider.realm()
+        try! realm.write {
+            event.desc = _desc
+        }
+        guard let ans = event.describe() else {
+            XCTFail()
+            return
+        }
+        XCTAssertTrue(ans.contains("And here are some extra descriptions:\n\n" + _desc))
+    }
+    
+    func testDescriptionForEmptyRecurringAllDayEvent() {
+        testTimeForAllDayEvent(recurring: true)
+        testDescriptionForExtraDescription(desc: nil)
+    }
+    
+    func testDescriptionForEmptyNonRecurringAllDayEvent() {
+        testTimeForAllDayEvent(recurring: false)
+        testDescriptionForExtraDescription(desc: nil)
+    }
+    
+    func testDescriptionForEmptyRecurringNonAllDayEvent() {
+        testTimeForNonAllDayEvent(recurring: true)
+        testDescriptionForExtraDescription(desc: nil)
+    }
+    
+    func testDescriptionForEmptyNonRecurringNonAllDayEvent() {
+        testTimeForNonAllDayEvent(recurring: false)
+        testDescriptionForExtraDescription(desc: nil)
+    }
+    
+    func testDescriptionForNonEmptyRecurringAllDayEvent() {
+        testTimeForAllDayEvent(recurring: true)
+        testDescriptionForExtraDescription(desc: "TEST")
+    }
+    
+    func testDescriptionForNonEmptyNonRecurringAllDayEvent() {
+        testTimeForAllDayEvent(recurring: false)
+        testDescriptionForExtraDescription(desc: "TEST")
+    }
+    
+    func testDescriptionForNonEmptyRecurringNonAllDayEvent() {
+        testTimeForNonAllDayEvent(recurring: true)
+        testDescriptionForExtraDescription(desc: "TEST")
+    }
+    
+    func testDescriptionForNonEmptyNonRecurringNonAllDayEvent() {
+        testTimeForNonAllDayEvent(recurring: false)
+        testDescriptionForExtraDescription(desc: "TEST")
     }
 }
